5a44038e51a9f91a758644d91a41ec8b576b4e29
cloudexpeditioners <cloudexpeditioners@gmail.com>
2026-05-26 23:35:40 -0500

    fix: random suffix for storage account name and contributor role

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -4,11 +4,29 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
 }
 
 provider "azurerm" {
   features {}
+}
+
+provider "random" {}
+
+# Generate random suffix for globally unique names
+resource "random_string" "suffix" {
+  length  = 6
+  lower   = true
+  numeric = true
+  special = false
+}
+
+locals {
+  storage_account_name_unique = "${var.storage_account_name}${random_string.suffix.result}"
 }
 
 # Resource Group
@@ -19,7 +37,7 @@ resource "azurerm_resource_group" "main" {
 
 # Storage Account
 resource "azurerm_storage_account" "main" {
-  name                     = var.storage_account_name
+  name                     = local.storage_account_name_unique
   resource_group_name      = azurerm_resource_group.main.name
   location                 = azurerm_resource_group.main.location
   account_tier             = "Standard"
