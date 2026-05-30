6d1757fa990ef29d3e8dfd1153dd6abb7fbec205
cloudexpeditioners <cloudexpeditioners@gmail.com>
2026-05-29 22:06:11 -0500

    fix: pin terraform version to 1.10.0

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -27,7 +27,7 @@ resource "azurerm_resource_group" "main" {
   }
 }
 
-# Storage Account
+# Storage Accounts
 resource "azurerm_storage_account" "main" {
   name                     = local.storage_account_name
   resource_group_name      = azurerm_resource_group.main.name
